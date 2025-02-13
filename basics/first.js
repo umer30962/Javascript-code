8634deb531a59fb25c3f5181730fc7136692d491
umer30962 <125677997+umer30962@users.noreply.github.com>
2025-02-13 18:24:30 +0000

    table done

--- basics/first.js
+++ basics/first.js
@@ -1,1 +1,14 @@
-console.log("Umer Ali")
+//console.log("Umer Ali")
+
+const account_id = 123
+let account_email = "umer@google.com"
+
+var account_pass = "789"
+
+let account_city = "Islamabad"
+
+console.log(account_id)
+
+
+
+console.table([account_email,account_id,account_pass,account_city])

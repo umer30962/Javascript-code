a000e5350aa4c3ee71dde9c17d38b057531717ea
umer30962 <125677997+umer30962@users.noreply.github.com>
2025-02-15 12:31:56 +0000

    Datatype Conversion

---
+++ basics/ConverstionOperation.js
@@ -0,0 +1,23 @@
+let score = 10;
+let Score = "10abc";
+console.log(typeof score)
+// console.log(typeof Score)
+
+
+let changeType = Number(Score)
+
+console.log(changeType)
+console.log(typeof changeType)
+
+
+
+let Temperature = true;
+
+console.log(Temperature)
+console.log (typeof Temperature)
+
+let LogIn = 1
+
+let BooleanLogin = Boolean(LogIn)
+
+console.log(BooleanLogin)

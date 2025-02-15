ee233e43ef879fd8ec8877ff19ebd54a13edc60d
umer30962 <125677997+umer30962@users.noreply.github.com>
2025-02-15 13:09:23 +0000

    Comaprison Operators

--- basics/ConverstionOperation.js
+++ basics/ConverstionOperation.js
@@ -1,23 +1,41 @@
-let score = 10;
-let Score = "10abc";
-console.log(typeof score)
-// console.log(typeof Score)
+// let score = 10;
+// let Score = "10abc";
+// console.log(typeof score)
+// // console.log(typeof Score)
 
 
-let changeType = Number(Score)
+// let changeType = Number(Score)
 
-console.log(changeType)
-console.log(typeof changeType)
+// console.log(changeType)
+// console.log(typeof changeType)
 
 
 
-let Temperature = true;
+// let Temperature = true;
 
-console.log(Temperature)
-console.log (typeof Temperature)
+// console.log(Temperature)
+// console.log (typeof Temperature)
 
-let LogIn = 1
+// let LogIn = 1
 
-let BooleanLogin = Boolean(LogIn)
+// let BooleanLogin = Boolean(LogIn)
 
-console.log(BooleanLogin)
+// console.log(BooleanLogin)
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~OPERATONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+let s1 =  "Hello"
+
+let s2 = " Umer"
+
+
+let s3 = s1 + s2
+
+console.log(s3);
+
+
+
+ 
---
+++ basics/comparison.js
@@ -0,0 +1,4 @@
+console.log ( 2 == 2)  // true
+console.log("2" == 2)  // true
+
+console.log("2"===2) // false
